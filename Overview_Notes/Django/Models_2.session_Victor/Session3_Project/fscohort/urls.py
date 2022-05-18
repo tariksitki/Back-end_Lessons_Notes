6a65e0350590eb719dd46be07b8dffe331c894e5
tarik <63872091+tariksitki@users.noreply.github.com>
2022-05-18 11:34:03 +0200

    Django templates preclass completed

--- Overview_Notes/Django/Models_2.session_Victor/Session3_Project/fscohort/urls.py
+++ Overview_Notes/Django/Models_2.session_Victor/Session3_Project/fscohort/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import home
+from .views import number_of_student, index
 
 urlpatterns = [
-    path("", home)
+     path("", index),
+     path("num/", number_of_student, name = "number_of_student")
 ]
--- Overview_Notes/Django/Models_2.session_Victor/Session3_Project/fscohort/views.py
+++ Overview_Notes/Django/Models_2.session_Victor/Session3_Project/fscohort/views.py
@@ -1,8 +1,23 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 
 
-def home(request):
-    return HttpResponse("Welcome to Homepage")
+# def home(request):
+#     return HttpResponse("Welcome to Homepage")
+
+def index(request):
+    return render(request, 'fscohort/home.html')
+
+
+def number_of_student(request):
+    students = Student.objects.all()
+    num_of_students = Student.objects.count()
+    context = {
+        'students' : students,
+        'num' : num_of_students
+    }
+
+    return render(request, 'fscohort/student_list.html', context)
